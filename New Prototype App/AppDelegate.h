078700e019c0f247711d5e124008dbd36d58b5d8
npwashuta16 <npwashuta16@TBSLTMBA814.local>
2015-02-07 10:47:40 -0600

    Initial Commit

---
+++ New Prototype App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  New Prototype App
+//
+//  Created by npwashuta16 on 2/7/15.
+//  Copyright (c) 2015 npwashuta16. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ New Prototype App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  New Prototype App
+//
+//  Created by npwashuta16 on 2/7/15.
+//  Copyright (c) 2015 npwashuta16. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
